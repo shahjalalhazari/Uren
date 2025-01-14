a8f4ad891c128da01f997a8b3eeaa4c5125032cf
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-01-14 11:57:45 +0400

    Offer banners added on home page dynamically.

--- src/app/components/HomePage/HomePage.jsx
+++ src/app/components/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import "./home-page.css";
 import CarouselBanner from "./CarouselBanner";
+import OfferBanners from "./OfferBanners";
 
 const HomePage = () => {
   return (
     <div>
       <CarouselBanner />
+      <OfferBanners deviceType="desktop" />
     </div>
   );
 };
