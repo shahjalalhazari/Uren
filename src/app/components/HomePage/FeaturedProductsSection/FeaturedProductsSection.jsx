8e9ea7b03f0fd6064d926e046926a0fe57e7cbcf
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2025-01-25 19:36:03 +0400

    Weekly On-Sale Products List Card added.

--- src/app/components/HomePage/FeaturedProductsSection/FeaturedProductsSection.jsx
+++ src/app/components/HomePage/FeaturedProductsSection/FeaturedProductsSection.jsx
@@ -1,5 +1,6 @@
 import DealOfTheDayCard from "./components/cards/DealOfTheDayCard";
 import MostViewedProductsCard from "./components/cards/MostViewedProductsCard";
+import OnSaleProductsCard from "./components/cards/OnSaleProductsCard";
 import WeeklyFeaturedProductsCard from "./components/cards/WeeklyFeaturedProductsCard";
 
 const FeaturedProductsSection = () => {
@@ -7,11 +8,15 @@ const FeaturedProductsSection = () => {
     <div className="px-8 py-20 bg-[#eee] grid grid-cols-4 gap-x-8">
       {/* 1st Card | Deal Of The Day */}
       <DealOfTheDayCard />
+
       {/* 2nd Card | Weekly Featured Products */}
       <WeeklyFeaturedProductsCard />
 
       {/* 3rd Card | Weekly Most Viewed Products */}
       <MostViewedProductsCard />
+
+      {/* 4th Card | On-Sale Products */}
+      <OnSaleProductsCard />
     </div>
   );
 };
---
+++ src/app/components/HomePage/FeaturedProductsSection/components/cards/OnSaleProductsCard.jsx
@@ -0,0 +1,23 @@
+import CardTitle from "../CardTitle";
+import FeaturedSingleProductCard from "../FeaturedSingleProductCard";
+
+const OnSaleProductsCard = () => {
+  return (
+    <div className="card-container">
+      {/* Card Heading */}
+      <CardTitle
+        heading={"On-Sale Products"}
+        subHeading={"On-Sale On This Week"}
+      />
+      {/* All Weekly Featured Products List */}
+      <div className="h-[75vh] overflow-y-auto mt-4">
+        {/* REPLACE */}
+        {[...Array(10)].map((_, index) => (
+          <FeaturedSingleProductCard key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OnSaleProductsCard;
